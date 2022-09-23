f5d40a0a61bddc9f494ddbc649e64b13bcfb4c72
Erick Nuque <erick@parametrics.ag>
2022-09-23 11:04:47 -0600

    spa in nextjs

--- learning_nextjs/pages/news/[newsSection].js
+++ learning_nextjs/pages/news/[newsSection].js
@@ -3,10 +3,27 @@
  * [] in the file name signifies that this is a dynamic page.
  * Can add any value inside the [] and this will be loaded on the page.
  * Any value after news/ will be rendered in the browser.
+ * This is a feature of nextjs to build truly dynamic web page.
  */
 
+/**
+ * To extract the concrete value in URL when dynamic page is loaded -
+ * special hook needs to be used - import useRouter from next/router - 
+ * then call this hook in the function and will have access to -
+ * router object with different data and methods and more importantly -
+ * the specific value of each dynamic pops segments.
+ */
+
+import {useRouter} from 'next/router';
 
 function NewsSectionPage () {
+
+    const router = useRouter();
+
+    // Access the concrete value of each dynamic page.
+    router.query.NewsSection;
+    // console.log(router.query.newsSection);
+
     return <h1>The News Section Page</h1>
 }
 
--- learning_nextjs/pages/news/index.js
+++ learning_nextjs/pages/news/index.js
@@ -1,5 +1,36 @@
+/**
+ * Single Page Application (SPA)
+ * Constructing a link in regular page and leverage the SPA.
+ * Fragment will be used to wrap around more that one tag 
+ * in the return statement - this is same with React.
+ * The difference is <></> - this React Fragment
+ * <Fragment></Fragment> - this is NextJS and this needs
+ * to be imported (not in React).
+ * Interactive UI in SPA.
+ */
+
+import Link from 'next/link'; // Responsible for SPA link for each
+import {Fragment} from 'react';
+
 function NewsPage () {
-    return <h1>The News Page</h1>
+    return (
+    <Fragment>
+     <h1>The News Page</h1>
+    <ul>
+        <li>
+            <Link href='/news/news1'>
+                News 1!
+            </Link>
+        </li>
+
+        <li>
+            <Link href='/news/news2'>
+                News 2!
+            </Link>
+        </li>
+    </ul>
+    </Fragment>
+    )
 }
 
 export default NewsPage;
