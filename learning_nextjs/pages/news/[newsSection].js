cf40dd8245a366c488c5eef2534f82f8e5f147cd
Erick Nuque <erick@parametrics.ag>
2022-09-23 10:24:37 -0600

    work on making dynamic page

---
+++ learning_nextjs/pages/news/[newsSection].js
@@ -0,0 +1,13 @@
+/**
+ * This is a dynamic page. 
+ * [] in the file name signifies that this is a dynamic page.
+ * Can add any value inside the [] and this will be loaded on the page.
+ * Any value after news/ will be rendered in the browser.
+ */
+
+
+function NewsSectionPage () {
+    return <h1>The News Section Page</h1>
+}
+
+export default NewsSectionPage;
